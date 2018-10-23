08100b04ec89e4c42702e4a23d522abbd41a85d1
rpbeltran <rp.beltran@yahoo.com>
2018-10-23 13:11:54 -0500

    error handling for installer

--- installer.py
+++ installer.py
@@ -1,17 +1,35 @@
 
-
-print( "Installing NLTK" )
-
-bashCommand = "pip install nltk"
-print( subprocess.check_output(['bash','-c', bashCommand]) )
+import subprocess, sys
 
 
 
-print( "Downloading required packages from NLTK" )
 
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
+print( "Installing NLTK..." )
+
+try:
+	bashCommand = "pip install nltk"
+	print( subprocess.check_output(['bash','-c', bashCommand]) )
+	import nltk
+
+except:
+	print( "Failed to install nltk. Make sure pip is installed.")
+	sys.exit()
+
+
+
+
+print( "\nDownloading required packages from NLTK..." )
+
+try:
+	nltk.download('punkt')
+	nltk.download('stopwords')
+	nltk.download('averaged_perceptron_tagger')
+	nltk.download('maxent_ne_chunker')
+	nltk.download('words')
+except:
+	print( "Failed to install required nltk packages.")
+	sys.exit()
+
+
+
+print( "\nSuccess!" )
